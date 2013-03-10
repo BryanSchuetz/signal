47e6327686bda2f23eed2f7f260418d1f0c6d625
Bryan Schuetz <bschuetz@gmail.com>
2013-03-10 18:34:14 -0400

    mo better

--- config.rb
+++ config.rb
@@ -88,12 +88,12 @@ set :markdown, :layout_engine => :erb,
                :autolink => true,
                :smartypants => true
 # Build-specific configuration
+set :build_dir, "/Volumes/ftp.signalbase.org"
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
   # Minify Javascript on build
   activate :minify_javascript
-
   # Enable cache buster
   # activate :cache_buster
 
